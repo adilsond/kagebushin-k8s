63109679e16ab7938267e871a8bf9cb6105c241d
Adilson dos Santos Dantas <adilson@adilson.net.br>
2020-02-10 23:01:57 -0300

    Adicionando data atual, normalmente em UTC

--- index.php
+++ index.php
@@ -6,4 +6,6 @@
 
 <h1>Ol&aacute;! Eu sou o pod <?php echo $_SERVER['SERVER_ADDR']; ?> &agrave; disposi&ccedil;&atilde;o.</h1></p>
 
+<p>Data: <?php echo date("D M j, Y,  G:i:s T "); ?></p>
+
 <p>D&uacute;vidas: <a href='mailto:adilson@adilson.net.br'>adilson@adilson.net.br</a></p>
