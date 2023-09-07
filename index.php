97d91c13ceddcff1e3fa875434bb0c5b34df0966
Adilson dos Santos Dantas <adilson@adilson.net.br>
2023-09-07 10:52:57 -0300

    Branch com phpinfo como exemplo para ver as configurações do PHP em uma imagem Docker.

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<title>Este &eacute; mais um deploy via Kubernetes</title>
+<?php
 
-<h1>&#24433;&#20998;&#36523;&#12398;&#34899;!</h1>
+phpinfo();
 
-<p>
-
-<h1>Ol&aacute;! Eu sou o pod <?php echo $_SERVER['SERVER_ADDR']; ?> &agrave; disposi&ccedil;&atilde;o.</h1></p>
-
-<p>Data: <?php echo date("D M j, Y,  G:i:s T "); ?></p>
-
-<p>D&uacute;vidas: <a href='mailto:adilson@adilson.net.br'>adilson@adilson.net.br</a></p>
+?>
