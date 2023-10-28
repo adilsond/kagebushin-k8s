9d9c8781d62179a7d65b7b5e52c895c4de4f9e6e
Adilson dos Santos Dantas <adilson@adilson.net.br>
2023-10-28 00:31:52 -0300

    Dando mais informações no index.php

--- index.php
+++ index.php
@@ -6,6 +6,25 @@
 
 <h1>Ol&aacute;! Eu sou o pod <?php echo $_SERVER['SERVER_ADDR']; ?> &agrave; disposi&ccedil;&atilde;o.</h1></p>
 
+<p><?php
+
+function exibirIP() {
+    if(isset($_SERVER['HTTP_X_REAL_IP']) && $_SERVER['HTTP_X_REAL_IP'] !== null) {
+        echo "Voc&ecirc; &eacute; " . $_SERVER['HTTP_X_REAL_IP'];
+    }
+}
+
+exibirIP();
+ ?></p>
+
+<p> Host do pod: <?php
+$hostname = gethostname();
+echo $hostname;
+?>
+</p>
+
 <p>Data: <?php echo date("D M j, Y,  G:i:s T "); ?></p>
 
 <p>D&uacute;vidas: <a href='mailto:adilson@adilson.net.br'>adilson@adilson.net.br</a></p>
+
+
