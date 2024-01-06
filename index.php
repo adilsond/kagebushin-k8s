9b36944e9698a268b159515f7988c294e32a31cd
Adilson dos Santos Dantas <adilson@adilson.net.br>
2024-01-06 01:06:13 -0300

    Indicando versão do PHP

--- index.php
+++ index.php
@@ -43,4 +43,4 @@ exibirIP();
 
 <p>D&uacute;vidas: <a href='mailto:adilson@adilson.net.br'>adilson@adilson.net.br</a></p>
 
-<p>Vers&atilde;o Debian</p>
+<p>Vers&atilde;o Debian com PHP da distribui&ccedil;&atilde;o</p>
